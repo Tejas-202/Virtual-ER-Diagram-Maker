a09902a0b0254acfe2d31c5fe6dc47f5212f7f65
Tejas Karanjavkar <93085260+Tejas-202@users.noreply.github.com>
2022-07-06 20:43:48 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # Construct ER diagram for library management system
 # Construct an ER diagram for a hospital with a set of patients and a set of medical doctors associatrd with each patient a log of various tests and examination conducted
 import cv2
+import numpy as np
 from cvzone.HandTrackingModule import HandDetector
 from question import *
 
@@ -12,9 +13,15 @@ cap.set(3, 1280)
 cap.set(4, 780)
 detector = HandDetector(detectionCon=0.8, maxHands=1)
 colorR = 225, 0, 225
+extraEntityCount = 0
+extraAttributeCount = 0
+extraRelationshipCount = 0
+horizontalLineCount = 0
+verticalLineCount = 0
+slantLineCount = 0
 
 
-completeQuestionEntityDataset = ["department", "manager", "employee", "projects", "hospital", "library", "doctors", "patient", "employee", "wards"]
+completeQuestionEntityDataset = ["department", "manager", "employee", "employees", "projects", "hospital", "library", "doctors", "patient", "employee", "wards"]
 incompleteQuestionEntitydataset = {
     "hospital": ["doctors", "patient", "employee", "wards"],
     "library": ["books", "employee", "customers"]
@@ -24,6 +31,7 @@ entityList = []
 attributeDataset = {
     "department": ["name", "id", "manager"],
     "manager": ["name", "id", "phone", "department"],
+    "employees": ["name", "id", "phone", "position"],
     "employee": ["name", "id", "phone", "position"],
     "company": ["name", "id"],
     "projects": ["name", "id", "duration", "employee"],
@@ -46,18 +54,14 @@ for word in questionSet:
             entityList.append(word)
 
 if len(entityList) == 1:
-    token = entityList[0];
+    token = entityList[0]
     entityList.pop(0)
-    entityList = incompleteQuestionEntitydataset.get(token);
-# else:
-#     token = entityList[0]
-#     if token in incompleteQuestionEntitydataset:
-#         entityList.pop(0)
+    entityList = incompleteQuestionEntitydataset.get(token)
+elif len(entityList) > 1:
+    token = entityList[0]
+    if token in incompleteQuestionEntitydataset:
+        entityList.pop(0)
 
-
-# entityList.sort()
-
-# print(entityList)
 
 
 class DragRect():
@@ -71,6 +75,45 @@ class DragRect():
 
         # if the index finger tip is in the rectangle region
         if cx - w // 2 < cursor[0] < cx + w // 2 and cy - h // 2 < cursor[1] < cy + h // 2:
+            self.posCenter = cursor
+
+class DragAttr():
+    def __init__(self, posCenter, size=[50, 30]):
+        self.posCenter = posCenter
+        self.size = size
+
+    def updatesAttr(self, cursor):
+        cx, cy = self.posCenter
+        w, h = self.size
+
+        # if the index finger tip is in the rectangle region
+        if cx - w // 2 < cursor[0] < cx + w // 2 and cy - h // 2 < cursor[1] < cy + h // 2:
+            self.posCenter = cursor
+
+class DragRel():
+    def __init__(self, posCenter, size=[80, 50]):
+        self.posCenter = posCenter
+        self.size = size
+
+    def updatesRel(self, cursor):
+        cx, cy = self.posCenter
+        w, h = self.size
+
+        # if the index finger tip is in the rectangle region
+        if cx - w // 2 < cursor[0] < cx + w // 2 and cy - h // 2 < cursor[1] < cy + h // 2:
+            self.posCenter = cursor
+
+class DragLine():
+    def __init__(self, posCenter, size=90):
+        self.posCenter = posCenter
+        self.size = size
+
+    def updatesLine(self, cursor):
+        cx, cy = self.posCenter
+        w = self.size
+
+        # if the index finger tip is in the rectangle region
+        if cx - 20 < cursor[0] < cx + 20 and cy - 20 < cursor[1] < cy + 20:
             self.posCenter = cursor
 
 
@@ -96,10 +139,33 @@ def drawAttributes(attributeList, cx, cy):
             cv2.arrowedLine(img, (cx, cy - 35), (cx + 60, cy - 65), colorR, 3)
 
 
+extraEntity = []
+extraEntityName = []
+extraAttribute = []
+extraAttributeName = []
+extraRelationship = []
+extraRelationshipName = []
+horizontalLines = []
+verticalLines = []
+slantLine = []
 
+for x in range(0, 10):
+    extraEntity.append(DragRect([400, 650]))
 
+for x in range(0, 10):
+    extraAttribute.append(DragAttr([400, 650]))
 
+for x in range(0, 10):
+    extraRelationship.append(DragRel([400, 650]))
 
+for x in range(0, 50):
+    horizontalLines.append(DragLine([400, 650]))
+
+for x in range(0, 50):
+    verticalLines.append(DragLine([400, 650]))
+
+for x in range(0, 50):
+    slantLine.append(DragLine([400, 650]))
 
 rectList = []
 for x in range(0, len(entityList)):
@@ -110,13 +176,14 @@ while True:
     success, img = cap.read()
     img = cv2.flip(img, 1)
     hand, _ = detector.findHands(img)
+
     if hand:
         hand1 = hand[0]
         lmList = hand1["lmList"]
         type = hand1["type"]
         # print(type)
-        a = detector.fingersUp(hand1)
-        print(a)
+        fingerUp = detector.fingersUp(hand1)
+        # print(a)
         # print(lmList)
         length, info = detector.findDistance((lmList[8][0], lmList[8][1]), (lmList[12][0], lmList[12][1]))
         # print(length)
@@ -126,6 +193,100 @@ while True:
             # call the update here
             for rect in rectList:
                 rect.updates(cursor)
+            for x in range(0, extraEntityCount):
+                rect = extraEntity[x]
+                rect.updates(cursor)
+            for x in range(0, extraAttributeCount):
+                attr = extraAttribute[x]
+                attr.updatesAttr(cursor)
+            for x in range(0, extraRelationshipCount):
+                rel = extraRelationship[x]
+                rel.updatesRel(cursor)
+            for x in range(0, horizontalLineCount):
+                line = horizontalLines[x]
+                line.updatesLine(cursor)
+            for x in range(0, verticalLineCount):
+                line = verticalLines[x]
+                line.updatesLine(cursor)
+            for x in range (0, slantLineCount):
+                line = slantLine[x]
+                line.updatesLine(cursor)
+
+        if type == "Right":
+            if fingerUp == [0, 1, 0, 0, 0]:
+                extraEntityCount = extraEntityCount + 1
+                name = input("Enter Entity Value: ")
+                extraEntityName.append(name)
+            if fingerUp == [0, 1, 1, 0, 0]:
+                extraAttributeCount = extraAttributeCount + 1
+                name = input("Enter Attribute Name: ")
+                extraAttributeName.append(name)
+            if fingerUp == [0, 0, 1, 1, 1]:
+                extraRelationshipCount = extraRelationshipCount + 1
+                name = input("Enter Relation: ")
+                extraRelationshipName.append(name)
+            if fingerUp == [0, 1, 1, 1, 1]:
+                horizontalLineCount = horizontalLineCount + 1
+                cv2.waitKey(500)
+            if fingerUp == [1, 0, 0, 0, 0]:
+                verticalLineCount = verticalLineCount + 1
+                cv2.waitKey(500)
+            if fingerUp == [0, 0, 0, 0, 1]:
+                slantLineCount = slantLineCount + 1
+                cv2.waitKey(500)
+
+
+
+    for x in range(0, extraEntityCount):
+        rect = extraEntity[x]
+        cx, cy = rect.posCenter
+        print(cx,cy)
+        w, h = rect.size
+        print(w,h)
+        cv2.rectangle(img, (cx - w // 2, cy - h // 2), (cx + w // 2, cy + h // 2), colorR, 3)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(img, extraEntityName[x], (cx - 80, cy), font, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
+
+    for x in range(0, extraAttributeCount):
+        ellp = extraAttribute[x]
+        cx, cy = ellp.posCenter
+        w, h = ellp.size
+        cv2.ellipse(img, (cx,cy), (w, h), 0, 0, 360, colorR, 3)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(img, extraAttributeName[x], (cx-30, cy+5), font, 0.6, (0, 225, 0), 2, cv2.LINE_AA)
+
+    for x in range(0, extraRelationshipCount):
+        rel = extraRelationship[x]
+        cx, cy = rel.posCenter
+        w, h = rel.size
+        pts = np.array([[cx-w, cy], [cx, cy-h], [cx+w, cy], [cx, cy+h]], np.int32)
+        pts = pts.reshape((-1, 1, 2))
+        isClosed = True
+        color = (255, 0, 0)
+        thickness = 2
+        cv2.polylines(img, [pts], isClosed, color, thickness)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(img, extraRelationshipName[x], (cx-30, cy+5), font , 0.6, (0, 225, 0), 2, cv2.LINE_AA)
+
+    for x in range(0, horizontalLineCount):
+        line = horizontalLines[x]
+        cx, cy = line.posCenter
+        w = line.size
+        cv2.line(img, (cx-w, cy), (cx+w, cy), colorR, 3)
+
+    for x in range(0, verticalLineCount):
+        line = verticalLines[x]
+        cx, cy = line.posCenter
+        w = line.size
+        cv2.line(img, (cx, cy+w), (cx, cy-w), colorR, 3)
+
+    for x in range(0, slantLineCount):
+        line = slantLine[x]
+        cx, cy = line.posCenter
+        w = line.size
+        cv2.line(img, (cx, cy+w), (cx-w, cy-w), colorR, 3)
+
+
     text = 0
     for rect in rectList:
 
@@ -137,7 +298,7 @@ while True:
         font = cv2.FONT_HERSHEY_SIMPLEX
         cv2.putText(img, entityList[text], (cx - 80, cy), font, 0.8, (0, 255, 0), 2, cv2.LINE_AA)
         if attributeDataset.get(entityList[text]):
-            #cv2.ellipse(img, (cx - 50, cy + 100), (50, 30), 0, 0, 360, colorR, 3)
+            # cv2.ellipse(img, (cx - 50, cy + 100), (50, 30), 0, 0, 360, colorR, 3)
             attributeList = attributeDataset.get(entityList[text])
             drawAttributes(attributeList, cx, cy)
         text = text + 1
